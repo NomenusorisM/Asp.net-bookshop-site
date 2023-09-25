af8428500af31e0a14a7dec5a0f8b3d6db855194
Максим Жупанов <maksim.jupanov@gmail.com>
2023-09-25 11:49:23 +0300

    Размечена примерная структура сайта

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,9 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About() => View();
+    public IActionResult Contact() => View();
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
