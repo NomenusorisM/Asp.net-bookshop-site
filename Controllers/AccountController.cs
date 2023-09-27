6f57067e3d49939eee4de6e048c68cbd2d81d483
NomenusorisM <maksim.jupanov@gmail.com>
2023-09-27 21:41:14 +0300

    Страница входа и контроллер для страниц аккаунта

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ASPNET.Models;
+
+namespace ASPNET.Controllers;
+public class AccountController : Controller
+{ 
+    public IActionResult Login() => View();
+    public IActionResult Registration() => View();
+    public IActionResult Profile() => View();
+        
+}
