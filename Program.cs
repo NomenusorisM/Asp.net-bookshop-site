2c2ae35f77d675549726b7d6dce8c7994e79375d
NomenusorisM <maksim.jupanov@gmail.com>
2023-10-08 22:34:41 +0300

    Перенос главной страницы в отдельную папку

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Main}/{action=Index}/{id?}");
 
 app.Run();
