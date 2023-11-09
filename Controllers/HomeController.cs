22c3649e30eddb88dc0c803c3d362d8e2bb34fb1
NomenusorisM <maksim.jupanov@gmail.com>
2023-11-09 21:25:08 +0300

    Подвязка данных бд к карточкам товаров

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,34 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BookShop.Models;
+using Microsoft.AspNetCore.Identity;
+using BookShop.Db;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BookShop.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ApplicationContext _context;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     [HttpGet]
     public IActionResult Index()
     {
-        return View();
+        return View(_context.Books.ToList());
     }
-
-    [HttpPost]
-    public string Index(string name, int age) => $"{name}: {age}";
 
     public IActionResult Privacy()
     {
         return View();
     }
+
 
     public IActionResult About() => View();
     public IActionResult Contact() => View();
@@ -34,5 +37,24 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult AddBook()
+    {
+        if (User.Identity.IsAuthenticated)
+            return View();
+        else throw new Exception();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddBook(Book book)
+    {
+        // Заглушка, в дальнейшем будет реализован выбор
+        book.AuthorId = 1;
+        book.Year = 0;
+        book.CategoryId = 1;
+        _context.Books.Add(book);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index", "Home");
     }
 }
--- Db/ApplicationContext.cs
+++ Db/ApplicationContext.cs
@@ -11,8 +11,8 @@ namespace BookShop.Db
             Database.EnsureCreated();
         }
 
-        public DbSet<Book> Books { get; }
-        public DbSet<Category> Categories { get; }
-        public DbSet<Author> Authors { get; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Author> Authors { get; set; }
     }
 }
